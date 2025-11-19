9f9df8ffaab143adb86e23ff57bb466baf8eee7d
Eric Joh <yongminjoh@gmail.com>
2025-11-19 03:18:07 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const config = require('./config/config');
 const {connectDB} = require('./config/db');
 
-const PORT = config.port || 4000;
+const PORT = config.port || 4001;
 
 // Connect to the database
 connectDB();
